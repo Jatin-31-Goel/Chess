8b01122426765d70c1dc250478e7baadaa39b5ab
Piyushkumar2004 <148059513+Piyushkumar2004@users.noreply.github.com>
2024-01-12 12:02:30 +0530

    Update King.cpp
    updated by Piyush on12th.

--- King.cpp
+++ King.cpp
@@ -14,12 +14,12 @@ class king{
         this->y = y;
         this->movements = 0;
     }
-    bool is_possible(int x,int y,chessboard chess)
+    bool is_possible(int x,int y,chessboard chess, int k)
     {
         int temp1 = abs(x - this->x);
         int temp2 = abs(y - this->y);
         
-        if((this->x==1 || this->x==8) && this->y==5 && this->movements==0 && (x==1 || x==8) && y==7)
+        if((this->x==1 || this->x==8) && this->y==5 && this->movements==0 && (x==1 || x==8) && y==7 && k==0)
         {
             if(chess.chess[x][y].c=="**" && chess.chess[x][y-1].c=="**")
                 return true;
@@ -38,60 +38,68 @@ class king{
         int temp = x - 1;
         while(temp>=1)
         {
-            cout<<"1";
+            //cout<<"1";
             if(chess.chess[temp][y].c[0]==chess.chess[x][y].c[0])
                 break;
-            else if(chess.chess[temp][y].c[0]!=chess.chess[x][y].c[0])
+            else if(chess.chess[temp][y].c[0]!=chess.chess[x][y].c[0] && !(chess.chess[temp][y].c == "**"))
             {
                 if(chess.chess[temp][y].c[1]=='R' || chess.chess[temp][y].c[1]=='Q')
                     return false;
                 else if(chess.chess[temp][y].c[1]=='K' && abs(temp - x) == 1)
                     return false;
+                else 
+                    break;
             }
             temp--;
         }
         temp = x + 1;
         while(temp<=8)
         {
-            cout<<"2";
+            //cout<<"2";
             if(chess.chess[temp][y].c[0]==chess.chess[x][y].c[0])
                 break;
-            else if(chess.chess[temp][y].c[0]!=chess.chess[x][y].c[0])
+            else if(chess.chess[temp][y].c[0]!=chess.chess[x][y].c[0] && !(chess.chess[temp][y].c == "**"))
             {
                 if(chess.chess[temp][y].c[1]=='R' || chess.chess[temp][y].c[1]=='Q')
                     return false;
                 else if(chess.chess[temp][y].c[1]=='K' && abs(temp - x) == 1)
                     return false;
+                else 
+                    break;
             }
             temp++;
         }
         temp = y - 1;
         while(temp>=1)
         {
-            cout<<"3";
+            //cout<<"3";
             if(chess.chess[x][temp].c[0]==chess.chess[x][y].c[0])
                 break;
-            else if(chess.chess[x][temp].c[0]!=chess.chess[x][y].c[0])
+            else if(chess.chess[x][temp].c[0]!=chess.chess[x][y].c[0] && !(chess.chess[temp][y].c == "**"))
             {
                 if(chess.chess[x][temp].c[1]=='R' || chess.chess[x][temp].c[1]=='Q')
                     return false;
                 else if(chess.chess[x][temp].c[1]=='K' && abs(temp - y) == 1)
                     return false;
+                else 
+                    break;
             }
             temp--;
         }
         temp = y + 1;
         while(temp<=8)
         {
-            cout<<"4";
+            //cout<<"4";
             if(chess.chess[x][temp].c[0]==chess.chess[x][y].c[0])
                 break;
-            else if(chess.chess[x][temp].c[0]!=chess.chess[x][y].c[0])
+            else if(chess.chess[x][temp].c[0]!=chess.chess[x][y].c[0] && !(chess.chess[temp][y].c == "**"))
             {
                 if(chess.chess[x][temp].c[1]=='R' || chess.chess[x][temp].c[1]=='Q')
                     return false;
                 else if(chess.chess[x][temp].c[1]=='K' && abs(temp - y) == 1)
                     return false;
+                else 
+                    break;
             }
             temp++;
         }
@@ -99,15 +107,17 @@ class king{
         int temp2 = y - 1;
         while(temp1>=1 && temp2>=1)
         {
-            cout<<"5";
+            //cout<<"5";
             if(chess.chess[temp1][temp2].c[0] == chess.chess[x][y].c[0])
                 break;
-            else if(chess.chess[temp1][temp2].c[0]!=chess.chess[x][y].c[0])
+            else if(chess.chess[temp1][temp2].c[0]!=chess.chess[x][y].c[0] && !(chess.chess[temp][y].c == "**"))
             {
                 if(chess.chess[temp1][temp2].c[1]=='B' || chess.chess[temp1][temp2].c[1]=='Q')
                     return false;
                 else if((chess.chess[temp1][temp2].c[1]=='K' || chess.chess[temp1][temp2].c[1]=='P') && (x - temp1) == 1 && (y - temp2) == 1)
                     return false;
+                else 
+                    break;
             }
             temp1--;
             temp2--;
@@ -116,15 +126,17 @@ class king{
         temp2 = y + 1;
         while(temp1<=8 && temp2<=8)
         {
-           cout<<"6";
+           //cout<<"6";
            if(chess.chess[temp1][temp2].c[0] == chess.chess[x][y].c[0])
                 break;
-            else if(chess.chess[temp1][temp2].c[0]!=chess.chess[x][y].c[0])
+            else if(chess.chess[temp1][temp2].c[0]!=chess.chess[x][y].c[0] && !(chess.chess[temp][y].c == "**"))
             {
                 if(chess.chess[temp1][temp2].c[1]=='B' || chess.chess[temp1][temp2].c[1]=='Q')
                     return false;
                 else if((chess.chess[temp1][temp2].c[1]=='K' || chess.chess[temp1][temp2].c[1]=='P') && (temp1 - x) == 1 && (temp2 - y) == 1)
                     return false;
+                else 
+                    break;
             }
             temp1++;
             temp2++;
@@ -133,15 +145,17 @@ class king{
         temp2 = y + 1;
         while(temp1>=1 && temp2<=8)
         {
-            cout<<"7";
+            //cout<<"7";
             if(chess.chess[temp1][temp2].c[0] == chess.chess[x][y].c[0])
                 break;
-            else if(chess.chess[temp1][temp2].c[0]!=chess.chess[x][y].c[0])
+            else if(chess.chess[temp1][temp2].c[0]!=chess.chess[x][y].c[0] && !(chess.chess[temp][y].c == "**"))
             {
                 if(chess.chess[temp1][temp2].c[1]=='B' || chess.chess[temp1][temp2].c[1]=='Q')
                     return false;
                 else if((chess.chess[temp1][temp2].c[1]=='K' || chess.chess[temp1][temp2].c[1]=='P') && (x - temp1) == 1 && (temp2 - y) == 1)
                     return false;
+                else 
+                    break;
             }
             temp1--;
             temp2++;
@@ -153,12 +167,14 @@ class king{
             cout<<"8";
             if(chess.chess[temp1][temp2].c[0] == chess.chess[x][y].c[0])
                 break;
-            else if(chess.chess[temp1][temp2].c[0]!=chess.chess[x][y].c[0])
+            else if(chess.chess[temp1][temp2].c[0]!=chess.chess[x][y].c[0] && !(chess.chess[temp][y].c == "**"))
             {
                 if(chess.chess[temp1][temp2].c[1]=='B' || chess.chess[temp1][temp2].c[1]=='Q')
                     return false;
                 else if((chess.chess[temp1][temp2].c[1]=='K' || chess.chess[temp1][temp2].c[1]=='P') && (temp1 - x) == 1 && (y - temp2) == 1)
                     return false;
+                else 
+                    break;
             }
             temp1++;
             temp2--;
