3a424512f3fa21bf13531fe119cc287b34500075
Jatin-31-Goel <148895480+Jatin-31-Goel@users.noreply.github.com>
2024-06-25 21:40:39 +0530

    Update Knight.cpp

--- Knight.cpp
+++ Knight.cpp
@@ -8,6 +8,7 @@ class knight{
     char color;
     int movements;
 
+    // Constructor with Parameters
     knight(char color, int x, int y){
         this->color = color;
         this->x = x;
@@ -15,18 +16,29 @@ class knight{
         this->movements = 0;
     }
 
+    // To check if Knight can move to the coordinates (x,y)
     bool is_possible(int x,int y,chessboard chess)
     {
+        // Ensuring that target spot (x,y) does not have the same color piece
         if(chess.chess[x][y].c[0] == chess.chess[this->x][this->y].c[0])
             return false;
-        int temp1 = abs(x - this->x);
-        int temp2 = abs(y - this->y);
-        if(temp1 ==1 && temp2 ==2)
+        
+        int diff_in_x = abs(x - this->x); // Finding the difference between the x-coordinates of the current and target spot 
+        int diff_in_y = abs(y - this->y); // Finding the difference between the y-coordinates of the current and target spot
+        
+        // Knight can only move : 
+        
+        if(diff_in_x ==1 && diff_in_y ==2) // Either one step horizontal and two steps vertical
             return true;
-        else if(temp1 ==2 && temp2 ==1)
+
+        else if(diff_in_x ==2 && diff_in_y ==1) // Or two steps horizontal and one step vertical
             return true;
-        return false;
+
+        else
+            return false;
     } 
+
+    // Changing the current coordinates to that of the target spot 
     void change_coordinates(int x,int y)
     {
         this->x = x;
